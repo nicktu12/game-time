64bd5fc9f9c529b3f3dc538f73d5a8287d5abc9d
Nick Teets <nickteets@gmail.com>
2017-08-15 18:19:09 -0600

    Create game class

--- main.bundle.js
+++ main.bundle.js
@@ -46,14 +46,7 @@
 
 	const Block = __webpack_require__(1);
 	const Paddle = __webpack_require__(2);
-
-	const canvas = document.getElementById('canvas');
-	const context = canvas.getContext('2d');
-
-	var block = new Block();
-
-	var buildAnArray = block.buildArray();
-	block.buildBlocks(buildAnArray, context);
+	const Game = __webpack_require__(3);
 
 /***/ }),
 /* 1 */
@@ -95,9 +88,56 @@
 /* 2 */
 /***/ (function(module, exports) {
 
-	console.log('paddle yo');
+	class Paddle {
+	  constructor(x, y) {
+	    this.x = x;
+	    this.y = y;
+	    this.width = 50;
+	    this.height = 10;
+	  }
+
+	  draw(context) {
+	    context.fillStyle = 'red';
+	    context.fillRect(this.x, canvas.height - this.height * 2.25, this.width, this.height);
+	  }
+
+	  moveRight() {
+	    this.x += 7;
+	  }
+
+	  moveLeft() {
+	    this.x -= 7;
+	  }
+	}
 
 	module.exports = Paddle;
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+	const canvas = document.getElementById('canvas');
+	const context = canvas.getContext('2d');
+
+	var block = new Block();
+
+	var paddle = new Paddle(100, 100);
+	paddle.draw(context);
+
+	var buildAnArray = block.buildArray();
+	block.buildBlocks(buildAnArray, context);
+
+	document.addEventListener('keydown', function (e) {
+	  if (e.keyCode === 39) {
+	    console.log(paddle.moveRight);
+	    paddle.moveRight();
+	  } else if (e.keyCode === 37) {
+	    console.log(27);
+	    paddle.moveLeft();
+	  }
+	});
+
+	module.exports = Game;
 
 /***/ })
 /******/ ]);
