cb18a2ed461d210a8776e5b0c7fb006bbaf5229e
Nick Teets <nickteets@gmail.com>
2017-08-20 15:29:32 -0600

    Initialize design, add background and update block class

---
+++ test/Block-test.js
@@ -0,0 +1,47 @@
+var { assert, expect, should } = require('chai');
+
+var Block = require('../lib/Block.js');
+var Ball = require('../lib/Ball.js');
+
+
+describe('block unit testing', function () {
+  it('should be a function', function () {
+    var block = new block();
+  })
+
+  it('should have a fixed width and height', function () {
+    var block = new block();
+
+    assert.equal(block.height === 10, true);
+    assert.equal(block.width === 50, true);
+  })
+
+  it('should build an array', function () {
+    var block = new block();
+    var blockArray = block.buildArray();
+
+    assert.equal(Array.isArray(blockArray), true);
+  })
+
+  it('should build an array of 24 block', function () {
+    var block = new block();
+    var blockArray = block.buildArray();
+
+    assert.equal(blockArray.length, 24);
+  })
+
+  it('should reverse direction of ball when colliding', function () {
+    var block = new block();
+    var ball = new Ball();
+    var blockArray = block.buildArray();
+
+    block.breakblock(blockArray, ball);
+
+
+
+    console.log('hiii');
+
+    assert.equal(1, 1)
+  })
+
+})
