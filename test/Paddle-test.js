1c4760792134a1fb7cfeb76dc8a800e51146ee4d
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-08-22 15:33:24 -0600

    refactoring in game and test files

--- test/Paddle-test.js
+++ test/Paddle-test.js
@@ -1,41 +1,45 @@
-var { assert, expect, should } = require('chai');
+const { assert } = require('chai');
 
-var Paddle = require('../lib/Paddle.js');
+const Paddle = require('../lib/Paddle.js');
 
 describe('paddle unit testing', () => {
   it('should be a function', () => {
-    var paddle = new Paddle();
+    let paddle = new Paddle();
   })
 
   it('should move right', () => {
-    var paddle = new Paddle(225, 476);
+    let paddle = new Paddle(225, 476);
     let paddleX = paddle.x;
+
     assert.equal(paddleX === 225, true);
     paddle.moveRight();
     assert.equal(paddle.x === paddleX + 10, true);
   })
 
   it('should move left', () => {
-    var paddle = new Paddle(225, 476);
+    let paddle = new Paddle(225, 476);
     let paddleX = paddle.x;
+
     assert.equal(paddleX === 225, true);
     paddle.moveLeft();
     assert.equal(paddle.x === paddleX - 10, true);
   })
 
   it('should not be able to go off the right side of the canvas', () => {
-    var paddle = new Paddle(440, 476);
+    let paddle = new Paddle(440, 476);
     let paddleLeft = paddle.x;
     let paddleRight = paddle.x + 50;
+
     assert.equal(paddleRight < 500, true);
     paddle.moveRight();
     assert.equal(paddleRight < 500, true);
   })
 
   it('should not be able to go off the left side of the canvas', () => {
-    var paddle = new Paddle(5, 476);
+    let paddle = new Paddle(5, 476);
     let paddleLeft = paddle.x;
     let paddleRight = paddle.x + 50;
+
     assert.equal(paddleLeft > 0, true);
     paddle.moveLeft();
     assert.equal(paddleLeft > 0, true);
